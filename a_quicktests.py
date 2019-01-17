3ca3b949acf623b9804457e771e8420000329756
Carl Page <carl.page@asmpt.com>
2019-01-17 17:39:08 +0000

    tests and squares

--- a_quicktests.py
+++ a_quicktests.py
@@ -1,28 +1,52 @@
 from backend.square import *
 
-def test_square_equality():
-    s1 = Square(0, 0)
-    s2 = Square(0, 1)
-    s3 = Square(0, 1)
-    s4 = Square(1, 1)
-    s5 = Square(1, 1)
-    assert(s1==s1)
-    assert(s1!=s2)
-    assert(s2==s3)
-    assert(s3!=s4)
-    assert(s4==s5)
+class AQuickTests:
+    def square_equality():
+        s1 = Square(0, 0)
+        s2 = Square(0, 1)
+        s3 = Square(0, 1)
+        s4 = Square(1, 1)
+        s5 = Square(1, 1)
+        assert(s1==s1)
+        assert(s1!=s2)
+        assert(s2==s3)
+        assert(s3!=s4)
+        assert(s4==s5)
 
-def square_copy_and_change():
-    s1 = Square(1, 1)
-    s2 = s1
-    assert(s1==s2)
-#    s2.x += 1
-#    assert(s1!=s2)
+    def square_offset():
+        s6 = Square(4, 4);
+        s7 = Square(2, 6);
+        s8 = s6.offset(-2, 2);
+        assert(s6 != s7);
+        assert(s7 == s8);
 
+    def square_copy():
+        s1 = Square(1, 1)
+        s2 = s1
+        assert(s1==s2)
 
-def tests():
-    test_square_equality()
-    square_copy_and_change()
+    def square_construct_from_notation():
+        assert( Square.from_notation("a1") == Square(1, 1))
+        assert( Square.from_notation("h1") != Square(1, 1))
+        assert( Square.from_notation("h1") == Square(8, 1))
+        assert( Square.from_notation("h8") == Square(8, 8))
+        assert( Square.from_notation("a8") == Square(1, 8))
 
-tests()
+    def square_to_string():
+        assert( str(Square.from_notation("a1")) == "a1" )
+        assert( str(Square.from_notation("h5")) == "h5" )
+
+    def square_subtraction():
+        s1 = Square(2, 5)
+        s2 = Square(6, 2)
+        v = s1 - s2
+        assert(v.x == -4)
+        assert(v.y == 3)
+        pass
+
+methods = [name for name in dir(AQuickTests) if callable(getattr(AQuickTests, name)) if not name.startswith('_')]
+for method in methods:
+    print(method)
+    getattr(AQuickTests, method)()
+
 print("All tests done")
--- backend/square.py
+++ backend/square.py
@@ -1,5 +1,30 @@
 from collections import namedtuple
 
-Square = namedtuple('Square', ['x', 'y'])
+Vector = namedtuple('Vector', ['x', 'y'])
 
+class Square(namedtuple('Square', ['x', 'y'])):
 
+    def in_bounds(self):
+        return self.x >= 1 and self.x <= 8 and self.y >= 1 and self.y <= 8
+
+    def from_notation( s ):
+        x = ord(s[0]) - 96
+        y = ord(s[1]) - 48
+
+        square = Square(x, y)
+        if( not square.in_bounds() ):
+            raise RuntimeError("Square out of bounds")
+        
+        return square
+
+    def __sub__(self, other):
+        return Vector( self.x - other.x, self.y - other.y )
+
+    def offset(self, dx, dy):
+        return Square( self.x + dx, self.y + dy)
+
+    def __str__(self):
+        if self.x < 1 or self.x > 8 or self.y < 1 or self.y > 8:
+            return "Invalid"
+        s = chr(self.x + 96) + chr(self.y + 48)
+        return s
